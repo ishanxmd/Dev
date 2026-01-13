9a0befd6144da88692df3f75942119ba217b2178
ishanxmd <ishanmadusanke548@gmail.com>
2026-01-13 22:22:17 +0530

    Update ALIVE_MSG to correct formatting

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "f5gy1AIR#FP5S8LFsoQSxuaBOu6Ok9D3dlY37AnI0Lwu2WZAyIpo"
 ALIVE_IMG : process.env.ALIVE_IMG || "https://files.catbox.moe/d0z6ym.JPG",
-ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃bot name 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ISHAN MD",
+ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃bot name 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : ISHAN-MD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
